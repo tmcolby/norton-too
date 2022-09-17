42ecaac980c7b43d073181a16a0c156eaaaa3265
tmcolby <tyson.colby@innotech.engineering>
2022-09-17 09:33:21 -0700

    Kip kiosk

--- kiosk.sh
+++ kiosk.sh
@@ -1,1 +1,2 @@
-chromium-browser --disable-infobars --kiosk 'http://localhost:3000/@signalk/instrumentpanel/'
+# chromium-browser --disable-infobars --kiosk 'http://localhost:3000/@signalk/instrumentpanel/'
+chromium-browser --disable-infobars 'http://localhost:3000/@mxtommy/kip/#/page/1'
